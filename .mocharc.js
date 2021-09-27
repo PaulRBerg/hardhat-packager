7a9b0793c71fbc2f181e095c46f2e858e2a97341
Paul Razvan Berg <hello@paulrberg.com>
2021-09-27 11:03:06 -0400

    chore: use "yaml" extension for .mocharc and .prettierrc

--- .mocharc.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = "test";
-
-module.exports = {
-  extension: ["ts"],
-  recursive: true,
-  require: ["earljs/mocha", "ts-node/register/transpile-only", "source-map-support/register"],
-  spec: ["test/**/*.test.ts"],
-  watchExtensions: ["ts"],
-};
